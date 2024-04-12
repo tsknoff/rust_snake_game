123dad437af172ceb3467c323e61f69937d3d6b8
Egor Tsukanov <ts.eg.99@gmail.com>
2024-04-12 16:45:01 +0200

    Add Nannou framework to the project
    - Include Nannou to enable graphics and event handling capabilities.
    - Set up a Nannou application with basic window settings and drawing configuration.
    - Configure the main window to be non-resizable and define frame rate control logic.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use nannou::prelude::*;
+
+const BLOCK_SIZE: u32 = 20;
+const CLIENT_WIDTH: u32 = BLOCK_SIZE * 20;
+const CLIENT_HEIGHT: u32 = BLOCK_SIZE * 20;
+const FRAMES_PER_MILLISECOND: u64 = 10;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .event(event)
+        .update(update)
+        .run();
+}
+
+struct Model {
+    _window: window::Id,
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window()
+        .size(CLIENT_WIDTH, CLIENT_HEIGHT)
+        .resizable(false)
+        .view(view)
+        .build()
+        .unwrap();
+
+    Model { _window }
+}
+
+fn update(app: &App, _model: &mut Model, _update: Update) {
+    if app.elapsed_frames() % FRAMES_PER_MILLISECOND != 0 {
+        return;
+    }
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    if app.elapsed_frames() % FRAMES_PER_MILLISECOND != 0 {
+        return;
+    }
+    let draw = app.draw();
+
+    draw.background().color(BLACK);
+
+    draw.to_frame(app, &frame).unwrap();
 }
